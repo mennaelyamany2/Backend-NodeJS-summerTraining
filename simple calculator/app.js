51f89dd3fae198e51de6b855aedc2ed2de282023
menna_elyamany <75741519+mennaelyamany2@users.noreply.github.com>
2023-07-19 12:05:59 +0300

    Add files via upload

---
+++ simple calculator/app.js
@@ -0,0 +1,19 @@
+let index = require('./index');
+let test =index.add(10,10);
+console.log(test);
+
+
+
+let test2=index.sub(10,5);
+console.log(test2);
+
+
+
+
+let test3 =index.multi(3,4);
+console.log(test3);
+
+
+
+let test4= index.div(10,2);
+console.log(test4);
---
+++ simple calculator/index.js
@@ -0,0 +1,20 @@
+module.exports = new class index{
+    add(x,y){
+        let res=x+y;
+        return res;
+    }
+    sub(x,y){
+        let res=x-y;
+        return res;
+    }
+    multi(x,y){
+        let res=x*y;
+        return res;
+
+    }
+    div(x,y){
+        let res=x/y;
+        return res;
+    }
+
+}
